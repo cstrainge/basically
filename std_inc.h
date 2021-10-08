d199a71c244aabaa4a4e9e4d7386ef09d0c634d3
Chloë Strainge <nullptr.0@gmail.com>
2021-10-08 14:28:25 -0700

    Cleaned up the system headers.

--- std_inc.h
+++ std_inc.h
@@ -4,37 +4,22 @@
 
 #include <cstddef>
 #include <optional>
+#include <string>
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <unordered_set>
 #include <unordered_map>
 #include <vector>
 #include <list>
+#include <tuple>
 #include <memory>
-#include <variant>
 #include <functional>
+#include <stdexcept>
+#include <compare>
 
 #include <libgccjit.h>
 
 
 using OptionalChar = std::optional<char>;
 using OptionalString = std::optional<std::string>;
-
-/*
-#include <stdexcept>
-#include <optional>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#include <list>
-#include <tuple>
-#include <memory>
-#include <string>
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include <compare>
-#include <variant>
-
-
-*/
