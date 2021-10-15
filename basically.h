e23e3d0b245b46f53545f65ce74d36ed7136e718
Chloë Strainge <nullptr.0@gmail.com>
2021-10-14 22:53:59 -0700

    Added include guard for basically.h

--- basically.h
+++ basically.h
@@ -1,49 +1,52 @@
 
-#include <cstddef>
-#include <optional>
-#include <iterator>
-#include <string>
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include <unordered_set>
-#include <unordered_map>
-#include <vector>
-#include <list>
-#include <tuple>
-#include <memory>
-#include <functional>
-#include <stdexcept>
-#include <compare>
-#include <variant>
-#include <filesystem>
-#include <cassert>
-
-#include <libgccjit.h>
+#ifndef BASICLY_INC_H
+#define BASICLY_INC_H
 
 
-namespace std
-{
+    #include <cstddef>
+    #include <optional>
+    #include <iterator>
+    #include <string>
+    #include <iostream>
+    #include <sstream>
+    #include <fstream>
+    #include <unordered_set>
+    #include <unordered_map>
+    #include <vector>
+    #include <list>
+    #include <tuple>
+    #include <memory>
+    #include <functional>
+    #include <stdexcept>
+    #include <compare>
+    #include <variant>
+    #include <filesystem>
+    #include <cassert>
+
+    #include <libgccjit.h>
 
 
-    namespace fs = filesystem;
+    namespace std
+    {
+        namespace fs = filesystem;
+    }
 
 
-}
+    using OptionalChar = std::optional<char>;
+    using OptionalString = std::optional<std::string>;
+    using OptionalSize = std::optional<size_t>;
+    using OptionalPath = std::optional<std::fs::path>;
+
+    using CompoundName = std::vector<std::string>;
 
 
-using OptionalChar = std::optional<char>;
-using OptionalString = std::optional<std::string>;
-using OptionalSize = std::optional<size_t>;
-using OptionalPath = std::optional<std::fs::path>;
+    #include "source.h"
+    #include "token.h"
+    #include "ast.h"
+    #include "parse.h"
+    #include "typing.h"
+    #include "variables.h"
+    #include "modules.h"
 
-using CompoundName = std::vector<std::string>;
 
-
-#include "source.h"
-#include "token.h"
-#include "ast.h"
-#include "parse.h"
-#include "typing.h"
-#include "variables.h"
-#include "modules.h"
+#endif
